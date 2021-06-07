c7865781d6750cd0e394498b085de9a2fbf2706d
renatombomio <72133002+renatombomio@users.noreply.github.com>
2021-06-07 12:45:35 +0200

    update

---
+++ CodigosCurso/01_inicial/js/01_holamundo.js
@@ -0,0 +1,46 @@
+//comentario again de una linea
+/* comentario multiple boy*/
+
+//console.time("iniciojs");
+//promesas
+//console.timeEnd("iniciojs")
+
+// variables
+// tipo(acceso) nombre = valor
+
+const NOMBRE= "Renato";
+console.log("Mi nombre es "+NOMBRE);
+var edad = 19;
+edad = 24;
+console.log("Tengo "+edad+" años de edad");
+let experiencia = true;
+
+let nombreString = "nombre";
+let edadNumber = 24;
+let edadNumberDecimal = 24.3;
+let experienciaBoolean = false;
+let cualquierCosaNull = null;
+let cosaSinDefinir = undefined;
+let cosaColeccion = ["uno", "dos", "tres", 1, true];
+let cosaColeccionMultiple = [
+    [1, 3],
+    [true, false],
+    [3.5, 7.9],
+];
+let variableObjeto = {nombre: "Renato", edad: "24", experiencia: true, hobbies: ["futbol", "musica"],};
+let fecha = new Date ();
+let palabraObject = new String("nombre objeto");
+
+//console.log("mi nombre es "+NOMBRE+" tengo "+edad+" años de edad y tengo experiencia"+experiencia);
+//console.log(`Mi nombre es ${NOMBRE} y tengo ${edad} años y mi experiencia es ${experiencia}`);
+//console.log("Mi nombre es %s y tengo %d y mi experiencia es %o",NOMBRE,edad,experiencia);
+
+
+console.log(typeof edadNumberDecimal);
+console.log(typeof nombreString);
+console.log(typeof cualquierCosaNull);
+console.log(typeof cosaSinDefinir);
+
+
+
+    
